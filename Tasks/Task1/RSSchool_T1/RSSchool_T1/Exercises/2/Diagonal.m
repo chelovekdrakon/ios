cd602bb09d713c700aea5088545b56a1c1538972
chelovekdrakon <MorauFiodar@yahoo.com>
2019-03-20 22:59:37 +0300

    * Math for Exercise 2 without Absolute difference

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -4,7 +4,24 @@
 
 // Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
-    return 0;
+    double primarySumm = 0;
+    double secondarySumm = 0;
+    
+    NSInteger faceLength = [array count];
+    
+    for (NSUInteger i = 0; i < faceLength; i++) {
+        NSString *string = [array objectAtIndex:i];
+        NSArray *subArray = [string componentsSeparatedByString: @" "];
+        
+        primarySumm += [[subArray objectAtIndex:i] doubleValue];
+        
+        NSUInteger secondaryIndex = (faceLength - 1) - i;
+        secondarySumm += [[subArray objectAtIndex:secondaryIndex] doubleValue];
+    }
+    
+    NSLog(@"hello!");
+    
+    return @(primarySumm - secondarySumm);
 }
 
 @end
