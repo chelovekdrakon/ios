839f5b6050f707b315d3709a9de55c40ab6cc459
chelovekdrakon <MorauFiodar@yahoo.com>
2019-03-22 23:04:45 +0300

    Exercise 4: done

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
@@ -4,7 +4,31 @@
 
 // Complete the encryption function below.
 - (NSString *)encryption:(NSString *)string {
-    return @"";
+    double stringLength = [string length];
+    double columnsAmount = ceil(sqrt(stringLength));
+    
+    NSMutableArray *resultArray = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < columnsAmount; i++) {
+        NSMutableString *str = [[NSMutableString alloc] init];
+        
+        for (int k = 0; k < columnsAmount; k++) {
+            NSUInteger position = ((columnsAmount * k) + i);
+            
+            if (position < stringLength) {
+                unichar ch = [string characterAtIndex:position];
+                NSString *letter = [NSString stringWithCharacters:&ch length:1];
+                
+                [str appendString:letter];
+            } else {
+                break;
+            }
+        }
+        
+        [resultArray addObject:str];
+    }
+    
+    return [resultArray componentsJoinedByString:@" "];
 }
 
 @end
