b71467de6ef4b9c97085a27bd67f66c4de3fcbf8
chelovekdrakon <MorauFiodar@yahoo.com>
2019-03-24 17:07:19 +0300

    Task1/exercise1: added retain and release for incoming array (funciton argument)

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
@@ -4,11 +4,15 @@
 
 // Complete the summArray function below.
 - (NSNumber *)summArray:(NSArray *)array {
+    [array retain];
+    
     double sum = 0;
     
     for (NSNumber *n in array) {
         sum += [n doubleValue];
     }
+    
+    [array release];
     
     return @(sum);
 }
