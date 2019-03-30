49757d3cc398f6395e0154565feca55a863e1d0b
chelovekdrakon <MorauFiodar@yahoo.com>
2019-03-30 22:43:01 +0300

    Task2/Exercise1: KidnapperNote done

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,33 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+
+- (BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note {
+    [magaine retain];
+    [note retain];
+    
+    BOOL result = YES;
+    
+    NSMutableString *mutableMagaine = [NSMutableString stringWithString:magaine];
+    [magaine release];
+    
+    NSArray *array = [note componentsSeparatedByString:@" "];
+    [note release];
+    
+    for (int i = 0; i < array.count; i++) {
+        NSString *word = array[i];
+        
+        NSRange range = [mutableMagaine localizedStandardRangeOfString:word];
+        
+        if (range.length == 0) {
+            result = NO;
+            break;
+        } else {
+            [mutableMagaine deleteCharactersInRange:range];
+        }
+    }
+    
+    return result;
+}
+
 @end
