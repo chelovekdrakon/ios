8d586d2a659fddb68183f705548697ea9b26aeaf
chelovekdrakon <MorauFiodar@yahoo.com>
2019-04-01 22:46:05 +0300

    Task2-Exercise3/refactoring: changed getDate method. setDateFormat:@"yyyy:MM:dd@ss\\mm/HH" works now

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/3/AssimilationInfo.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/3/AssimilationInfo.m
@@ -19,31 +19,10 @@
 @synthesize seconds = _seconds;
 
 - (NSDate *)getDate:(NSString *)dateString {
-    NSRange rangeOfYear = NSMakeRange(0, 4);
-    NSString *yearStr = [dateString substringWithRange:rangeOfYear];
-    
-    NSRange rangeOfMonth = NSMakeRange(5, 2);
-    NSString *monthStr = [dateString substringWithRange:rangeOfMonth];
-    
-    NSRange rangeOfDay = NSMakeRange(8, 2);
-    NSString *dayStr = [dateString substringWithRange:rangeOfDay];
-    
-    NSRange rangeOfHours = NSMakeRange([dateString length] - 2, 2);
-    NSString *hoursStr = [dateString substringWithRange:rangeOfHours];
-    
-    NSRange rangeOfMinutes = NSMakeRange([dateString length] - 5, 2);
-    NSString *minutesStr = [dateString substringWithRange:rangeOfMinutes];
-    
-    NSRange rangeOfSeconds = NSMakeRange([dateString length] - 8, 2);
-    NSString *secondsStr = [dateString substringWithRange:rangeOfSeconds];
-    
-    NSString *stringDateFormatted = [NSString stringWithFormat:@"%@:%@:%@ %@:%@:%@",
-                                     dayStr, monthStr, yearStr, hoursStr, minutesStr, secondsStr];
-    
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"dd:MM:yyyy HH:mm:ss"];
+    [dateFormatter setDateFormat:@"yyyy:MM:dd@ss\\mm/HH"];
     
-    NSDate *date = [dateFormatter dateFromString:stringDateFormatted];
+    NSDate *date = [dateFormatter dateFromString:dateString];
     
     [dateFormatter release];
     
@@ -55,6 +34,8 @@
     [dateFormatter setDateFormat:@"EEEE, MMMM dd, yyyy, HH:mm:ss"];
     
     NSDate *asimilationDate = [dateFormatter dateFromString:@"Sunday, August 14, 2208, 03:13:37"];
+    
+    [dateFormatter release];
     
     return asimilationDate;
 }
@@ -82,18 +63,6 @@
     }
     
     return self;
-}
-
-- (void)dealloc {
-//    _years = nil;
-//    _months = nil;
-//    _weeks = nil;
-//    _days = nil;
-//    _hours = nil;
-//    _minutes = nil;
-//    _seconds = nil;
-    
-    [super dealloc];
 }
 
 @end
