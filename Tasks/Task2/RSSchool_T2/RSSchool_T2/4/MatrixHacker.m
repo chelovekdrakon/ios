db081e8e0755ac38abb2a8da378eb807bb93c675
chelovekdrakon <MorauFiodar@yahoo.com>
2019-04-01 22:52:22 +0300

    Task2-Exercise4/fix: added retain release for method arg: (NSArray *)names

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
@@ -14,12 +14,16 @@ typedef id<Character> (^HackerBlock)(NSString *name);
 }
 
 - (NSArray<id<Character>> *)runCodeWithData:(NSArray<NSString *> *)names {
+    [names retain];
+    
     NSMutableArray *result = [NSMutableArray new];
     
     for (NSString *name in names) {
         id<Character> ch = self.hackerBlock(name);
         [result addObject:ch];
     }
+    
+    [names release];
     
     return [result autorelease];
 }
