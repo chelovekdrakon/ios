e7cbcd1fca6ce3bd166c45fbc323f322c69538fa
chelovekdrakon <MorauFiodar@yahoo.com>
2019-04-14 16:48:29 +0300

    Task3-Exercise1/feature: added method to parseArray using recursion. Done

--- Tasks/RSSchool_T3/RSSchool_T3/1/ArrayPrint.h
+++ Tasks/RSSchool_T3/RSSchool_T3/1/ArrayPrint.h
@@ -15,6 +15,7 @@
 
  For all the others, it should print `unsupported`
  */
+
 @interface NSArray (RSSchool_Extension_Name)
 - (NSString *)print;
 @end
--- Tasks/RSSchool_T3/RSSchool_T3/1/ArrayPrint.m
+++ Tasks/RSSchool_T3/RSSchool_T3/1/ArrayPrint.m
@@ -1,1 +1,34 @@
 #import "ArrayPrint.h"
+
+@implementation NSArray (RSSchool_Extension_Name)
+
+- (NSString *)print {
+    return [self parseArray:self];
+}
+
+- (NSString *)parseArray:(NSArray *)array {
+    NSMutableArray *resultArray = [[NSMutableArray alloc] init];
+    
+    for (NSObject *object in array) {
+        if ([object isKindOfClass:[NSArray class]]) {
+            [resultArray addObject:[self parseArray:(NSArray *)object]];
+        } else if ([object isKindOfClass:[NSNumber class]]) {
+            NSString *str = [NSString stringWithFormat:@"%@", object];
+            [resultArray addObject: str];
+        } else if ([object isKindOfClass:[NSNull class]]) {
+            [resultArray addObject: @"null"];
+        } else if ([object isKindOfClass:[NSString class]]) {
+            NSString *str = [NSString stringWithFormat:@"\"%@\"", object];
+            [resultArray addObject: str];
+        } else {
+            [resultArray addObject: @"unsupported"];
+        }
+    }
+    
+    NSString *result = [resultArray componentsJoinedByString:@","];
+    [resultArray release];
+    
+    return [NSString stringWithFormat:@"[%@]", result];
+}
+
+@end
